0569249adb3c06d97ea04e79000b47d592f3d70a
Amit123103 <amitakhil001@gmail.com>
2026-07-24 13:50:54 +0530

    fix: remove duplicate size_t typedef from stdint.h

--- include/stdint.h
+++ include/stdint.h
@@ -9,7 +9,6 @@ typedef signed char int8_t;
 typedef signed short int16_t;
 typedef signed int int32_t;
 typedef signed long long int64_t;
-typedef unsigned long size_t;
 
 typedef unsigned long uintptr_t;
 
